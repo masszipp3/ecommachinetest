35090663f843422b59ea5d9887c767cdd87fdbe5
rishan <masszippyc@gmail.com>
2023-09-20 02:57:28 +0530

    'first2'

--- Ecom_test/settings.py
+++ Ecom_test/settings.py
@@ -139,8 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='masszippyab@gmail.com'
-EMAIL_HOST_PASSWORD='svqimtrqwuckrhpv'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
